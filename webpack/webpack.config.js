12c5000ffafe331fbfb79f2432ccc4e90764b802
emmmwang <954544828@qq.com>
2022-02-19 13:23:26 +0800

    copy-webpack-plugin 复制文件

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,7 +1,14 @@
 const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 // 独立加载css文件插件
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { dirname } = require('path');
+const { DefinePlugin } = require('webpack')
+
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 
 module.exports = {
@@ -20,6 +27,32 @@ module.exports = {
             // linkType: 'text/css',
             // 开启禁用生成css 仍将被提取, 并可用自定义加载方法
             runtime: false
+        }),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            // 这个插件会自动将bundle.js 注入到 html 文件中 
+            // 呢么 在index.html 中就不用自己引入 了
+            template: path.resolve(__dirname, './public/index.html'),
+            filename: 'index.html',
+            // 设置注入位置
+            inject: 'head',
+            title: '王小王',
+            minify: {
+                removeComments: true
+            }
+        }), new DefinePlugin({
+            BASE_URL: "'./'"
+        }), new CopyWebpackPlugin({
+            patterns: [{
+                from: 'public',
+                // 当前目录是 output 中设置的 path
+                to: './img/',
+                globOptions: {
+                    ignore: [
+                        '**/index.html'
+                    ]
+                }
+            }]
         })
     ],
     module: {
