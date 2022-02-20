56c68484f2cf1900ad452295689bc23a4f90fc0c
emmmwang <954544828@qq.com>
2022-02-21 02:16:08 +0800

    父组件向子组件传值

--- 14_learn_component/src/03_父组件传递子组件/App.vue
+++ 14_learn_component/src/03_父组件传递子组件/App.vue
@@ -1,16 +1,18 @@
 <template>
     <div>
-        <show-message title="emmm" content="王小王"></show-message>
+        <show-message id="jyy" class="wang" title="emmm" content="王小王"></show-message>
         <show-message :title="title" :content="content"></show-message>
         <show-message :title="message.title" :content="message.content"></show-message>
-        <show-message :="message"></show-message>
+        <show-message :="message" ></show-message>
+        <muilti-root-element id="lmm"></muilti-root-element>
     </div>
 </template>
 
 <script>
+import MuiltiRootElement from './MuiltiRootElement.vue'
 import ShowMessage from './ShowMessage.vue'
 export default {
-    components: { ShowMessage },
+    components: { ShowMessage, MuiltiRootElement },
     data(){
         return {
             title:'蒋莹莹',
--- 14_learn_component/src/03_父组件传递子组件/ShowMessage.vue
+++ 14_learn_component/src/03_父组件传递子组件/ShowMessage.vue
@@ -1,13 +1,42 @@
 <template>
     <div>
-        <h2>{{title}}</h2>
+        <!-- 绑定 非 props 中的属性 -->
+        <!-- $attrs :继承传过来的 所有 属性 -->
+        <!-- :class="$attrs.class" 继承传过来的 class 属性 ... -->
+        <h2 :='$attrs' >{{title}}</h2>
         <p>{{content}}</p>
+        <h2>{{counter}}</h2>
     </div>
 </template>
 
 <script>
     export default {
-        props:['title','content']
+        // 这种写法无法对父组件传来的值进行限制
+        // props:['title','content']
+
+        inheritAttrs:false,
+        props:{
+            title:String,
+            content:{
+                type:String,
+                // 必传
+                // required:true,
+                // 默认值
+                default(){
+                    return '王小王'
+                }
+
+            },
+            counter:{
+                type:String,
+                // required:true,
+                // default:'王小王'
+                // 如果是引用类型 默认值必须是 返回一个函数, 每次调用都新创建一个对象!
+                default(){
+                    return '王小王'
+                }
+            }
+        }
     }
 </script>
 
