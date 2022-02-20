6faeb9da54ff959ce393a653ec3cbce2be47e781
emmmwang <954544828@qq.com>
2022-02-21 03:24:44 +0800

    子组件向父组件传值

---
+++ 14_learn_component/src/04_子组件传递父组件/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h2>当前计数为:{{counter}}</h2>
+        <counter-operation @addN="incrementN" @add="addOne" @sub="subOne"></counter-operation>
+    </div>
+</template>
+
+<script>
+import CounterOperation from './CounterOperation.vue'
+    export default {
+  components: { CounterOperation },
+        data(){
+            return {
+                counter:0
+            }
+        },methods:{
+            addOne(){
+                this.counter++;
+            },
+            subOne(){
+                this.counter--;
+            },
+            incrementN(num){
+                console.log(typeof(num))
+                this.counter=this.counter+num
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ 14_learn_component/src/04_子组件传递父组件/CounterOperation.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <button @click="increment">+1</button>
+        <button @click="decrement">-1</button>
+
+        <input type="text" v-model="num">
+        <button @click="incrementN">+n</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        // emits:['add','sub'],
+        // 对象写法的目的是为了进行参数的验证
+        emits:{
+            add:null,
+            sub:null,
+            addN:(num,name)=>{
+                // console.log(num) ;
+                // console.log('---'+typeof(num));     
+                // console.log(name);
+
+                // 对参数进行验证
+                if(num>10){
+                    return true;
+                }else{
+                    return false;
+                }
+         }
+        },
+        data(){
+            return {
+                num:0
+            }
+        },
+        methods:{
+            increment(){
+                console.log('+1');
+                this.$emit('add');
+            },
+            decrement(){
+                console.log('-1');
+                this.$emit('sub');
+            },
+            incrementN(){
+                this.$emit('addN',parseInt(this.num),'fda')
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
