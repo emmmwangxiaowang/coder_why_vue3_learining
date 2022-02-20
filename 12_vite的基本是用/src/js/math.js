7f8750fe79eac6b7650c599c905895ac678e892e
emmmwang <954544828@qq.com>
2022-02-20 19:29:40 +0800

    浏览器 对 ES Module的支持

---
+++ 12_vite的基本是用/src/js/math.js
@@ -0,0 +1,1 @@
+export function sum(num1, num2) { return num1 + num2 }
---
+++ 12_vite的基本是用/src/main.js
@@ -0,0 +1,6 @@
+import { sum } from './js/math.js'
+import _ from '../node_modules/lodash-es/lodash.default.js'
+
+console.log('hello world')
+console.log(sum(1, 2))
+console.log(_.join(['abc', 'bca'], '-'))
