b6f95704c6c43cef8e890714be0c3b2987939ef8
emmmwang <954544828@qq.com>
2022-02-21 01:18:30 +0800

    父->子 组件传值

---
+++ 14_learn_component/src/03_父组件传递子组件/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <show-message title="emmm" content="王小王"></show-message>
+        <show-message :title="title" :content="content"></show-message>
+        <show-message :title="message.title" :content="message.content"></show-message>
+        <show-message :="message"></show-message>
+    </div>
+</template>
+
+<script>
+import ShowMessage from './ShowMessage.vue'
+export default {
+    components: { ShowMessage },
+    data(){
+        return {
+            title:'蒋莹莹',
+            content:'李墁墁',
+            message:{
+                title:'陈昌霖',
+                content:'廖明月'
+            }
+        }
+    } 
+}
+</script>
+
+<style scoped>
+
+</style>
