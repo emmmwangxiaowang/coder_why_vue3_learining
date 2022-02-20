a00e6e52fb837b9857ae8eca397391b3e6c53cfd
emmmwang <954544828@qq.com>
2022-02-20 23:40:37 +0800

    组件的拆分与嵌套

---
+++ 14_learn_component/src/01_组件的拆分和嵌套/Main.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="main">
+        <main-banner></main-banner>
+        <main-product-llist></main-product-llist>
+    </div>
+</template>
+
+<script>
+import MainBanner from './MainBanner.vue'
+import MainProductLlist from './MainProductLlist.vue'
+    export default {
+    components: { MainBanner, MainProductLlist },
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
