52c16b2c6a8527f7e35531611c7620fd99fec978
emmmwang <954544828@qq.com>
2022-02-21 01:07:34 +0800

    css 作用域

---
+++ 14_learn_component/src/02_组件中的css作用域/App.vue
@@ -0,0 +1,22 @@
+<template>  
+    <div>
+        <!-- 如果不加 根元素, scoped 不会起作用, 会 '越界' -->
+        <h2>App</h2>
+        <hello-world></hello-world>
+    </div>
+    
+</template>
+
+<script>
+import HelloWorld from './HelloWorld.vue'
+    export default {
+  components: { HelloWorld },
+        
+    }
+</script>
+
+<style scoped>
+    h2{
+        color: cyan;
+    }
+</style>
--- 14_learn_component/src/main.js
+++ 14_learn_component/src/main.js
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './01_组件的拆分和嵌套/App.vue'
+import App from './02_组件中的css作用域/App.vue'
 
 createApp(App).mount('#app')
