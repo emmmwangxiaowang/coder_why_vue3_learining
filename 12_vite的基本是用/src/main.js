b2188dbe15025bc3c013f8695b67be2ad8ef8177
emmmwang <954544828@qq.com>
2022-02-20 20:23:26 +0800

    vite 基本使用

--- 12_vite的基本是用/src/main.js
+++ 12_vite的基本是用/src/main.js
@@ -1,6 +1,18 @@
-import { sum } from './js/math.js'
-import _ from '../node_modules/lodash-es/lodash.default.js'
+import { sum } from './js/math'
+import _ from 'lodash-es'
+import './css/style.css'
+import './css/title.less'
+
+import { createApp } from 'vue'
+import App from './vue/App.vue'
 
 console.log('hello world')
 console.log(sum(1, 2))
 console.log(_.join(['abc', 'bca'], '-'))
+
+const titleEl = document.createElement('div')
+titleEl.className = 'title'
+titleEl.innerHTML = 'hello vite'
+document.body.appendChild(titleEl)
+
+createApp(App).mount('#app')
---
+++ 12_vite的基本是用/src/vue/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h2>{{message}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            message:'hello world'
+        }
+    }    
+        
+}
+</script>
+
+<style scoped>
+    h2{
+        color: white;
+    }
+</style>
+
