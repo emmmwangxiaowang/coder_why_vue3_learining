81373d615927c12503e07b7e024ce47c915719a1
emmmwang <954544828@qq.com>
2022-02-21 17:42:09 +0800

    独占默认插槽

--- 14_learn_component/src/10_作用域插槽的使用/App.vue
+++ 14_learn_component/src/10_作用域插槽的使用/App.vue
@@ -7,9 +7,18 @@
         </child-cpn>
 
         <shwo-names :names="names">
+            <!-- 插槽名不写 默认为 default -->
+            <!-- #="slotProps" 是 v-slot:=slotProps 缩写 -->
+            <!-- 这个 是获取 插槽 定义方 传过来的值 -->
             <template #="slotProps">
                <button>{{slotProps.item}}--{{slotProps.index}}</button>
             </template>
+        </shwo-names>
+
+        <!-- 独占默认插槽缩写 只有默认插槽, 不是具名插槽 -->
+        <!-- 如果又其它的具名插槽, 呢么默认插槽也必须使用template来编写 -->
+        <shwo-names :names="names" #="slotProps">
+               <button>{{slotProps.item}}--{{slotProps.index}}</button>
         </shwo-names>
         
         <shwo-names :names="names">
