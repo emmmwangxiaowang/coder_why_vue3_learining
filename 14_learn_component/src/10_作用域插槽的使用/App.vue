ec08d266e5c63d497b26bee8af77b7784447582f
emmmwang <954544828@qq.com>
2022-02-21 17:33:30 +0800

    作用域插槽的使用

--- 14_learn_component/src/10_作用域插槽的使用/App.vue
+++ 14_learn_component/src/10_作用域插槽的使用/App.vue
@@ -5,16 +5,30 @@
             <!-- 但是 其中标签访问的数据 就是本 标签定义 组件中的数据 -->
             <button>{{title}}</button>
         </child-cpn>
+
+        <shwo-names :names="names">
+            <template #="slotProps">
+               <button>{{slotProps.item}}--{{slotProps.index}}</button>
+            </template>
+        </shwo-names>
+        
+        <shwo-names :names="names">
+            <template #="slotProps">
+               <strong>{{slotProps.item}}--{{slotProps.index}}</strong>
+            </template>
+        </shwo-names>
     </div>
 </template>
 
 <script>
-import ChildCpn from './ChildCpn.vue'
+    import ChildCpn from './ChildCpn.vue'
+import ShwoNames from './ShwoNames.vue'
     export default {
-    components: { ChildCpn },
+    components: { ChildCpn, ShwoNames },
     data(){
         return {
-            title:"wang"
+            title:"wang",
+            names:['a','b','c','d']
         }
     }
         
