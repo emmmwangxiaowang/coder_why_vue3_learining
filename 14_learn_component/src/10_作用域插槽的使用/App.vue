15326b93276968a8526cfb794fb84880bc8307dc
emmmwang <954544828@qq.com>
2022-02-21 17:21:45 +0800

    vue 的渲染作用域

---
+++ 14_learn_component/src/10_作用域插槽的使用/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <child-cpn>
+            <!-- 虽然插槽 中的 标签 最后是放到 子组件中 -->
+            <!-- 但是 其中标签访问的数据 就是本 标签定义 组件中的数据 -->
+            <button>{{title}}</button>
+        </child-cpn>
+    </div>
+</template>
+
+<script>
+import ChildCpn from './ChildCpn.vue'
+    export default {
+    components: { ChildCpn },
+    data(){
+        return {
+            title:"wang"
+        }
+    }
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
