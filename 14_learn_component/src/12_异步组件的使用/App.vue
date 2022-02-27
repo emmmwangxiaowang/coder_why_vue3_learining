2727a2e567551a9f8c8ecd61144c5f6ae430631f
emmmwang <954544828@qq.com>
2022-02-27 13:36:07 +0800

    webpack import 函数分包

---
+++ 14_learn_component/src/12_异步组件的使用/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        App 组件
+        <home></home>
+    </div>
+</template>
+
+<script>
+    import Home from '../06_Provide和inject使用/Home.vue'
+    
+    export default {
+    components: { Home },
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
