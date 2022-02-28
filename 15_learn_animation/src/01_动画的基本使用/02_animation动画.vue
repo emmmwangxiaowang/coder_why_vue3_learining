d5f39bead45c0e7c7488cecfa4fec7e9d2288f83
emmmwang <954544828@qq.com>
2022-02-28 13:34:44 +0800

    transition的两个属性 type 和 duration

--- 15_learn_animation/src/01_动画的基本使用/02_animation动画.vue
+++ 15_learn_animation/src/01_动画的基本使用/02_animation动画.vue
@@ -6,12 +6,7 @@
         
 
         <!-- transition 组件会自动在合适的时候将对应的类 添加/删除 到组件中 -->
-
-        <!-- 当 transition 和 animation 的时间不一样的时候, 在 transition 的属性里面加一个type属性 设置为 时间较长的一个 -->
-        <!-- :duration 显式指定动画时间 可以设置两种类型的值 -->
-        <!-- number 类型: 同时设置进入和离开的过度时间 -->
-        <!-- object 类型: 分别设置进入和离开的过度时间 {enter:800,leave:500} -->
-        <transition name="wang" type="transition" :duration="1000">
+        <transition name="wang">
             <h2 class="title" v-if="isShow">hello world</h2>
         </transition>
     </div>
@@ -42,16 +37,6 @@
     #app{
         width:200px;
         margin:0 auto;
-    }
-
-    .wang-enter-from,
-    .wang-leave-to{
-        opacity: 0;
-    }
-
-    .wang-enter-active,
-    .wang-leave-active{
-        transition: opacity 2s ease
     }
 
     .wang-enter-active
