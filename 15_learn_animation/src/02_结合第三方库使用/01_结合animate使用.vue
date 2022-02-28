ef306c1cc6538f7ae71228b883e000a15160082d
emmmwang <954544828@qq.com>
2022-02-28 21:32:38 +0800

    生命周期钩子 及 gasp 使用

---
+++ 15_learn_animation/src/02_结合第三方库使用/01_结合animate使用.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <div>
+            <button @click="change">显示|隐藏</button><br>
+        </div>
+        
+
+        <!-- mode 有两个模式 in-out   out-in -->
+        <!-- in-out 先出现, 再退出, 会出现多个元素 -->
+        <!-- out-in 先退出,再出现 仅出现一个元素-->
+        <transition enter-active-class="animate__animated animate__fadeInDown"
+                    leave-active-class="animate__animated animate__flipInY">
+            <h2 class="title" v-if="isShow">emmm</h2>
+        </transition>
+    </div>
+</template>
+
+<script>
+  
+
+    export default {
+        
+        data(){
+            return {
+                isShow:true
+            }
+        },
+        methods:{
+            change(){
+                this.isShow=!this.isShow
+                console.log(this.isShow)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+    .title{
+        display: inline-block;
+    }
+    
+    .animate__flipInY{
+        animation-direction: reverse;
+    }
+    
+</style>
---
+++ 15_learn_animation/src/02_结合第三方库使用/02_生命周期钩子结合 gsap使用.vue
@@ -0,0 +1,72 @@
+<template>
+    <div>
+        <div>
+            <button @click="change">显示|隐藏</button><br>
+        </div>
+        
+        <transition @before-enter="beforeEnter"
+                    @enter="enter"
+                    @after-enter="afterEnter"
+                    @before-leave="beforeLeave"
+                    @leave="leave"
+                    @afterLeave="afterLeave"
+                    :css="false">
+            <h2 class="title" v-if="isShow">emmm</h2>
+        </transition>
+    </div>
+</template>
+
+<script>
+    import gsap from 'gsap'
+
+
+    export default {
+        data(){
+            return {
+                isShow:true
+            }
+        },
+        methods:{
+            change(){
+                this.isShow=!this.isShow
+                console.log(this.isShow)
+            },
+            beforeEnter(){
+                console.log('beforeEnter');
+                
+            },enter(el,done){
+                console.log('enter');
+                gsap.from(el,{
+                    scale:0,
+                    x:200,
+                    onComplete:done
+                })
+            },afterEnter(el,done ){
+                console.log('afterEnter');
+                gsap.to(el,{
+                    scale:1,
+                    x:200,
+                    onComplete:done
+                })
+            },beforeLeave(){
+                console.log('beforeLeave');
+            },leave(){
+                console.log('leave');
+            },afterLeave(){
+                console.log('afterLeave');
+            },
+        }
+    }
+</script>
+
+<style scoped>
+
+    .title{
+        display: inline-block;
+    }
+    
+    .animate__flipInY{
+        animation-direction: reverse;
+    }
+    
+</style>
