aed47fce6069a7eda3bbdd51e23e65af8101409c
emmmwang <954544828@qq.com>
2022-02-28 23:09:14 +0800

    transition-group 的移动和重排

---
+++ 15_learn_animation/src/02_结合第三方库使用/03_gsap 数字递增动画.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="app">
+        <input type="number" step="100" v-model="counter">
+        <h2>{{showCounter}}</h2>
+        <h2>{{showNumber.toFixed(0)}}</h2>
+    </div>
+</template>
+
+<script>
+    import gsap from 'gsap'
+
+
+    export default {
+        data(){
+            return {
+                counter:0,
+                showNumber:0
+            }
+        },
+        methods:{},
+        computed:{
+            showCounter(){
+                // toFixed 中的参数 表示 小数点后面的位数
+                return this.showNumber.toFixed(0)
+            }
+        },
+        watch:{
+            counter(newValue){
+                // this.showNumber=newValue
+                gsap.to(this,{
+                    duration:1,showNumber:newValue
+                })
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+   
+</style>
