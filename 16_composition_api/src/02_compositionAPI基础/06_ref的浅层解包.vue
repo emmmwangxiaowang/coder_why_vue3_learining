89596c9ac9300342d5299890aa0102e46a10673c
emmmwang <954544828@qq.com>
2022-03-02 23:29:37 +0800

    readonly 的使用

--- 16_composition_api/src/02_compositionAPI基础/06_ref的浅层解包.vue
+++ 16_composition_api/src/02_compositionAPI基础/06_ref的浅层解包.vue
@@ -3,11 +3,16 @@
         <h2>{{message}}</h2>
         <!-- 再 template模板中使用 ref 对象时,会自动解包 -->
         <h2>当前计数:{{counter}}</h2>
+           <!-- ref 的解包 是浅层解包(info 是一个普通的js对象) -->
+        <h2>{{info}}</h2>
+        <!-- 当最外层包裹的是一个 reactive 可响应式对象, 呢么 内容的ref 可以解包 -->
+        <h2>{{reactiveInfo.counter}}</h2>
         <button @click="increment">+1</button>
     </div>
 </template>
 
 <script>
+import {reactive} from 'vue'
 import {ref} from 'vue'
     export default {
         props:{
@@ -27,17 +32,27 @@ import {ref} from 'vue'
         setup() {
           
           let counter=ref(100)
-
-            let info=ref(counter)
+            // optionsAPI 使用 reactive 来提供响应式
+            // const state= reactive({
+            //     counter:100,
+            //     title:'hello'
+            // })
+            let info=ref({counter})
             // 局部函数
+
+            const reactiveInfo=reactive({
+                counter
+            })
             const increment=()=>{
                 // 再模板中会自动解包, 但是再方法中不会
                 counter.value++;
                 console.log(counter);
             }
             return {
-                 counter,
-                 increment
+                reactiveInfo,
+                info,
+                counter,
+                increment
             }
         }
     }
