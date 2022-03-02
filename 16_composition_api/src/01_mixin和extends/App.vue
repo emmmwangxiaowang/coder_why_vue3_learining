47f48d1f37aafb911f54d70ff61439da577b00de
emmmwang <954544828@qq.com>
2022-03-02 21:23:03 +0800

    extends的使用

--- 16_composition_api/src/01_mixin和extends/App.vue
+++ 16_composition_api/src/01_mixin和extends/App.vue
@@ -2,26 +2,15 @@
   <div>
     <h2>{{message}}</h2>
     <button @click="foo">按钮</button>
+    <about></about>
   </div>
 </template>
 
 <script>
-import {demoMixin} from './mixins/demoMixin'
+  import About from './pages/About.vue'
 export default {
-    mixins:[demoMixin],
-  data(){
-    return {
-      message:"hello world"
-    }
-  },
-  methods:{
-    // 方法重名会覆盖, 以组件中为主
-    foo(){
-      console.log('app foo');
-    }
-  },
-  created(){
-    console.log("App created 执行了");
+  components:{
+    About
   }
 }
 </script>
---
+++ 16_composition_api/src/01_mixin和extends/pages/About.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        about
+        <h2>{{title}}</h2>
+        <button @click="bar">按钮</button>
+    </div>
+</template>
+
+<script>
+import BasePage from './BasePage.vue'
+    export default {
+        extends:BasePage,
+        data(){
+            return {
+                content:"hello about"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
