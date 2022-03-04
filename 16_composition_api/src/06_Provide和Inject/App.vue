17618424bdcbf1cd93bd4c0aef0102ec95f6446b
emmmwang <954544828@qq.com>
2022-03-04 13:10:37 +0800

    provide和inject

---
+++ 16_composition_api/src/06_Provide和Inject/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <h2>{{counter}} </h2>
+        <home></home>
+        <button @click="increment">counter++</button>
+    </div>
+</template>
+
+<script>
+import {ref,provide,readonly} from 'vue'
+import Home from './Home.vue'
+export default {
+  components: { Home },
+    setup () {
+       Home   
+       
+
+        // 开发中 尽量用 ref , reactive 不好抽离
+        const name=ref("wang")
+        let counter=ref(100)
+
+        // 父组件 提供的数据用 readonly 包裹, 防止 子组件修改数据
+        provide("name",readonly(name))
+        provide("counter",readonly(counter))
+
+        const increment=()=>{
+            counter.value++
+            console.log(counter)
+        }
+
+        return {
+            increment,
+            counter,
+            name
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ 16_composition_api/src/06_Provide和Inject/Home.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h2>home</h2>
+        <button @click="homeIncrement">home+1</button>
+
+        <h2>{{name}}</h2>
+        <h2>{{counter}}</h2>
+
+    </div>
+</template>
+
+<script>
+import {inject} from 'vue'
+    export default {
+        setup(props) {
+            const name = inject("name","jiang");
+            const counter = inject("counter")
+
+            const homeIncrement=()=>{
+                counter.value++
+            }
+
+            return {
+                name,
+                counter,
+                homeIncrement
+            }
+          
+            
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
