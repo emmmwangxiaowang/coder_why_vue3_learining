68e307ece9deee4890742f6a283c8052e4d38d99
emmmwang <954544828@qq.com>
2022-03-17 18:23:14 +0800

    setup的另一种使用方式

---
+++ 16_composition_api/src/08_setup顶层编写方式/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h2>当前技术: {{counter}}</h2>
+        <button @click="increment">+1</button>
+
+        <hello-world message="wang" @increment="getCounter"></hello-world>
+    </div>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+import HelloWorld from './HelloWorld.vue'
+    const counter=ref(0);
+    const increment=()=>counter.value++;
+
+    const getCounter=(payload)=>{
+        console.log(payload);
+          
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ 16_composition_api/src/08_setup顶层编写方式/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h2>hello world</h2>
+        <h2>{{message}}</h2>
+        <button @click="emitEvent">发送事件</button>
+    </div>
+</template>
+
+<script setup>
+    import {defineProps,defineEmits} from 'vue'
+
+    const props = defineProps({
+        message:{
+            type:String,
+            default:"emmm"
+        }
+    })
+
+    const emit=defineEmits(["increment","decrement"]);
+    const emitEvent=()=>{
+        emit('increment',"100000");
+    }
+
+</script>
+
+<style scoped>
+
+</style>
