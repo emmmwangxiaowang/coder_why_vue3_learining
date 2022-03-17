630e22cd7c67ce9976b49b6cfa48196646303ae8
emmmwang <954544828@qq.com>
2022-03-17 18:27:54 +0800

    render函数的基本使用

---
+++ 16_composition_api/src/09_render函数的使用/App.vue
@@ -0,0 +1,15 @@
+<script>
+
+    import {h} from 'vue'
+
+    export default{
+        render(){
+            return h("h2",{class:"title"},"emmm")
+        }
+    }
+
+</script>
+
+<style  scoped>
+
+</style>
