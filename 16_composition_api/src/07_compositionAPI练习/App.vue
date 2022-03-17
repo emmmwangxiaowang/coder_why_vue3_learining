19f01f6ba973e3856efe99146bcaa4fe93d60d15
emmmwang <954544828@qq.com>
2022-03-17 17:41:59 +0800

    compositionAPI练习

--- 16_composition_api/src/07_compositionAPI练习/App.vue
+++ 16_composition_api/src/07_compositionAPI练习/App.vue
@@ -5,12 +5,21 @@
         <button @click="increment">+1</button>
         <button @click="decrement">-1</button>
 
+        <h2>{{data}}</h2>
+            <button @click="changeData">修改data</button>
+
         <p class="content"></p>
 
         <div class="scroll">
             <div class="scroll-x">scroll-x:{{scrollX}}</div>
             <div class="scroll-y">scroll-y:{{scrollY}}</div>
         </div>
+
+        <div class="mouse">
+            <div class="mouse-x">mouseX:  {{mouseX}}</div>
+            <div class="mouse-y">mouseX:  {{mouseY}}</div>
+        </div>
+
     </div>
 </template>
 
@@ -19,6 +28,8 @@
 import useCounter from './hooks/useCounter.ts'
 import useTitle from './hooks/useTitle.ts'
 import useScrollPosition from './hooks/useScrollPosition.ts'
+import useMousePosition from './hooks/useMousePosition.ts'
+import useLocalStorage from './hooks/useLocalStorage.ts'
 
 export default {
     setup () {
@@ -31,15 +42,33 @@ export default {
         }, 2000);
 
 
+       // 滚动位置
        const{scrollX,scrollY}=useScrollPosition();
 
+        // 鼠标位置
+        const {mouseX,mouseY}=useMousePosition();
+
+        const changeData=()=>{
+            data.value="emmm"
+        }
+
+        // localStorage
+        const data = useLocalStorage("info")
         return {
             counter,
             doubleCounter,
+
             increment,
             decrement,
+
             scrollY,
-            scrollX
+            scrollX,
+
+            mouseX,
+            mouseY,
+
+            data,
+            changeData
         }
     }
 }
@@ -55,5 +84,11 @@ export default {
         position: fixed;
         right: 30px;
         bottom: 30px;
+    }
+
+    .mouse{
+        position: fixed;
+        right:30px;
+        bottom:80px;
     }
 </style>
