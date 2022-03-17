542319b85b49feb2f7b053c9b77108b1fa02aa76
emmmwang <954544828@qq.com>
2022-03-17 21:15:16 +0800

    jsx的使用

---
+++ 17_other_composition/src/02_jsx的使用/HelloWorld.vue
@@ -0,0 +1,17 @@
+
+<script>
+    export default {
+        render(){
+            return (
+                <div>
+                    <h2>hello world</h2>    
+                    {this.$slots.default ? this.$slots.default() : <span>emmm</span>}
+                </div>
+            )
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
